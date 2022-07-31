4d81e2e0c767ad432750df1215fbac79cf9fae3c
soladobola <mladen.fix@gmail.com>
2022-07-31 14:20:48 +0200

    added state stuf

--- TablicExtreme/src/main/java/agent/State.java
+++ TablicExtreme/src/main/java/agent/State.java
@@ -1,6 +1,35 @@
 package agent;
 
+import game.Card;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class State {
+
+    ArrayList<Card> hand;
+    HashMap<String, ArrayList<Card>> history;
+    ArrayList<Card> board;
+
+    int enemyCardCount;
+
+    public ArrayList<State> generateAllStates(){
+        return null;
+    }
+
+
+    // [1, 5, 8, 4, 2]
+    //[11, ...]
+
+   // 13
+
+   // => [[1, 2], [1, 2, 3], [0, 4]]
+
+
+    // 8
+    // 2, 3, 4, 5, .. n
+    //(8, 2) + (8, 3) + (8, 8)
+
+
 
 
 
