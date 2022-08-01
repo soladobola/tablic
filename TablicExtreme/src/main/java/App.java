5b3d01abe16cfa7f98bd588db9264eabef6263b8
soladobola <mladen.fix@gmail.com>
2022-08-01 12:57:14 +0200

    permutations with Aces added.

--- TablicExtreme/src/main/java/App.java
+++ TablicExtreme/src/main/java/App.java
@@ -1,18 +1,41 @@
+import agent.State;
 import game.Deck;
 import game.util.DeckUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
         Deck deck = new Deck();
-        int count = 0;
-        while(true) {
-            System.in.read();
-            count++;
-            System.out.println(deck.takeCard());
-            System.out.println(count);
-        }
+        State state = new State();
+
+        state.board.add(deck.takeCard());
+        state.board.add(deck.takeCard());
+        state.board.add(deck.takeCard());
+        state.board.add(deck.takeCard());
+
+        System.out.println(state.board);
+
+        List<List<Integer>> values = state.permuteAces(state.board);
+
+        System.out.println(values);
+
+
+        /*
+       ArrayList<Integer> values = new ArrayList<>();
+       values.add(1);
+       values.add(2);
+       values.add(3);
+       values.add(4);
+
+
+
+       System.out.println(state.subsetSum(values, 4));
+        */
 
 
     }
--- TablicExtreme/src/main/java/agent/State.java
+++ TablicExtreme/src/main/java/agent/State.java
@@ -3,12 +3,13 @@ package agent;
 import game.Card;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class State {
 
-    ArrayList<Card> hand;
-    HashMap<String, ArrayList<Card>> history;
-    ArrayList<Card> board;
+    public ArrayList<Card> hand;
+    public HashMap<String, ArrayList<Card>> history;
+    public ArrayList<Card> board;
 
     int enemyCardCount;
 
@@ -16,21 +17,69 @@ public class State {
         return null;
     }
 
-
-    // [1, 5, 8, 4, 2]
-    //[11, ...]
-
-   // 13
-
-   // => [[1, 2], [1, 2, 3], [0, 4]]
+    public State(){
+        hand = new ArrayList<>();
+        board = new ArrayList<>();
+    }
 
 
-    // 8
-    // 2, 3, 4, 5, .. n
-    //(8, 2) + (8, 3) + (8, 8)
+    public List<List<Integer>> subsetSum(List<Integer> values, int target){
+
+        List<List<Integer>> solution = new ArrayList<>();
+        int n = values.size();
+
+        for (int i = 0; i < (1<<n); i++)
+        {
+            ArrayList<Integer> temp = new ArrayList<>();
+            int m = 1;
+            int tempSum = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if ((i & m) > 0)
+                {
+                    temp.add(j);
+                    tempSum += values.get(j);
+                    if(tempSum > target) break;
+                }
+                m = m << 1;
+            }
+            if(tempSum == target) solution.add(temp);
+        }
+
+        return solution;
+    }
 
 
+    public List<List<Integer>> permuteAces(ArrayList<Card> board){
+        List<List<Integer>> result = new ArrayList<>();
 
+        ArrayList<Integer> acesInd = new ArrayList<>();
+        ArrayList<Integer> values = new ArrayList<>();
 
+        for(int i = 0; i < board.size(); i++){
+            values.add(board.get(i).getValue().get(0));
+            if(board.get(i).getSymbol().equals("A")) acesInd.add(i);
+        }
+
+        changeAces(result, values, acesInd, 0);
+        return result;
+    }
+
+    private void changeAces(List<List<Integer>> result, List<Integer> values, List<Integer> indexes, int current){
+        if(current >= indexes.size()){
+            result.add(values);
+            return;
+        }
+
+        List<Integer> branch1 = new ArrayList<>(values);
+        List<Integer> branch2 = new ArrayList<>(values);
+
+        branch1.set(indexes.get(current), 1);
+        branch2.set(indexes.get(current), 11);
+
+        changeAces(result, branch1, indexes, current + 1);
+        changeAces(result, branch2, indexes, current + 1);
+
+    }
 
 }
